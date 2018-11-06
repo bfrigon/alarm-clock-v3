91a1e682f115e327c4c04e3399564103a1b2d2c0
Benoit Frigon <benoit@frigon.info>
2018-11-06 00:27:19 -0500

    Updated mechanical drawings

--- drawings/mechanical/README.md
+++ drawings/mechanical/README.md
@@ -1,30 +1,34 @@
 Index
 -----
 
-Doc ID        | Part #            | Description
+Document ID   | Part #            | Description
 --------------|-------------------|-------------------------------------------------
 CLKV3-DWG-01  | CLKV3-PC-01       | Display - RGB digit PCB
 CLKV3-DWG-02  | CLKV3-ML-01       | Display - Frame - Front
-CLKV3-DWG-03  | CLKV3-ML-02       | Display - Back cover
-CLKV3-DWG-04  | CLKV3-ML-03       | Display - Front cover
-CLKV3-DWG-05  | CLKV3-PC-02       | Display - RGB dots PCB
-CLKV3-DWG-06  | CLKV3-PC-03       | Touch keypad PCB
-CLKV3-DWG-07  | CLKV3-ML-04       | Touch keypad overlay
-CLKV3-DWG-08  | CLKV3-ML-09       | Display - Frame - Back
+CLKV3-DWG-03  | CLKV3-ML-03       | Display - Front cover
+CLKV3-DWG-04  | CLKV3-ML-09       | Display - Frame - Back
+CLKV3-DWG-05  | CLKV3-ML-02       | Display - Back cover
+CLKV3-DWG-06  | CLKV3-PC-02       | Display - RGB dots PCB
+CLKV3-DWG-07  | CLKV3-PC-03       | Touch keypad PCB
+CLKV3-DWG-08  | CLKV3-ML-04       | Touch keypad overlay
 CLKV3-DWG-09  | CLKV3-PC-08       | RGB lamp PCB
-
-
-CLKV3-DWG-10  | CLKV3-ML-05       | Enclosure - lower section
-CLKV3-DWG-11  | CLKV3-ML-06       | Enclosure - upper section
-CLKV2-DWG-12  | CLKV3-PC-04       | Front panel PCB
+CLKV3-DWG-10  | CLKV3-ML-05       | Enclosure - Lower section
+CLKV3-DWG-11  | CLKV3-ML-06       | Enclosure - Upper section
+CLKV3-DWG-12  | CLKV3-PC-04       | Front panel PCB
+CLKV3-DWG-13  | CLKV3-ML-10       | Enclosure - Back panel
 CLKV3-DWG-14  | CLKV3-PC-05       | Motherboard PCB
 CLKV3-DWG-15  | CLKV3-PC-06       | Daughterboard PCB
-
-CLKV3-DWG-20  | CLKV3-AS-01       | ASSY - Display frame - front
-CLKV3-DWG-21  | CLKV3-AS-02       | ASSY - Display frame - back
-CLKV3-DWG-22  | CLKV3-AS-03       | ASSY - Display frame - front
-
-
-CLKV3-DWG-21  | CLKV3-AS-02       | ASSY - Motherboard
-CLKV3-DWG-23  | CLKV3-AS-04       | ASSY - Touch keypad
-CLKV3-DWG-30  | CLKV3             | Final assembly
+CLKV3-DWG-16  | CLKV3-PC-07       | PSU PCB
+CLKV3-DWG-17  | CLKV3-PC-09       | Alarm switch PCB
+CLKV3-DWG-18  | CLKV3-ML-07       | RTC Battery door
+CLKV3-DWG-19  | CLKV3-ML-08       | RGB lamp window - Frame
+CLKV3-DWG-20  | CLKV3-ML-09       | RGB lamp window - Cover
+CLKV3-DWG-21  | CLKV3-ML-11       | Front panel window
+CLKV3-DWG-30  | CLKV3-AS-01       | ASSY - Display frame - Front
+CLKV3-DWG-31  | CLKV3-AS-02       | ASSY - Display frame - Back
+CLKV3-DWG-32  | CLKV3-AS-03       | ASSY - Display
+CLKV3-DWG-33  | CLKV3-AS-04       | ASSY - Motherboard PCB
+CLKV3-DWG-34  | CLKV3-AS-05       | ASSY - Touch keypad
+CLKV3-DWG-35  | CLKV3             | Final assembly
+CLKV3-DWG-50  | CLKV3-JG-01       | Jig - dev.
+CLKV3-DWG-51  | CLKV3-JG-02       | Motherboard assembly jig
--- drawings/mechanical/merge.sh
+++ drawings/mechanical/merge.sh
@@ -5,19 +5,19 @@ TMP_PDF_OUTPUT="/tmp/_merge_sh_output.pdf"
 
 
 change_drawing_title () {
-    
+
     git diff-index --quiet --exit-code HEAD $1.pdf
-    
+
     #### Check if the file was modified ####
     if [ $? -eq 1 ]; then
-    
+
         echo "InfoBegin" > $TMP_METADATA_FILE
         echo "InfoKey: Title" >> $TMP_METADATA_FILE
         echo "InfoValue: $2" >> $TMP_METADATA_FILE
         echo "InfoBegin" >> $TMP_METADATA_FILE
         echo "InfoKey: Author" >> $TMP_METADATA_FILE
         echo "InfoValue: Benoit Frigon" >> $TMP_METADATA_FILE
-    
+
         pdftk $1.pdf update_info $TMP_METADATA_FILE output $TMP_PDF_OUTPUT
         cp $TMP_PDF_OUTPUT $1.pdf
     fi
@@ -27,30 +27,30 @@ combine_drawing_files () {
 
     input_files=""
     current_page=1
-    
+
     echo > $TMP_METADATA_FILE
 
     for drawing_id in $(echo $2 | tr "," "\n")
     do
         #### Dump metainfo from pdf ####
-        meta=$(pdftk clkv3-dwg-$drawing_id.pdf dump_data)
-        
+        meta=$(pdftk $1-$drawing_id*.pdf dump_data)
+
         #### Extract title and number of pages info ####
         title=$(echo "$meta" | sed -n '/InfoKey/ {  N ; s/InfoKey: Title\nInfoValue: \(.*\)/\1/p }')
         num_pages=$(echo "$meta" | sed -n 's/NumberOfPages: \(.*\)/\1/p')
-        
+
         #### Add current drawing to the bookmark data ####
         echo "BookmarkBegin" >> $TMP_METADATA_FILE
         echo "BookmarkTitle: $title" >> $TMP_METADATA_FILE
         echo "BookmarkLevel: 1" >> $TMP_METADATA_FILE
         echo "BookmarkPageNumber: $current_page" >> $TMP_METADATA_FILE
-        
-        input_files="$input_files clkv3-dwg-$drawing_id.pdf"
+
+        input_files="$input_files $1-$drawing_id*.pdf"
 
         current_page=$(($current_page+$num_pages))
     done
-    
-    
+
+
     pdftk $input_files cat output $TMP_PDF_OUTPUT
     pdftk $TMP_PDF_OUTPUT update_info $TMP_METADATA_FILE output $1.pdf
 }
@@ -60,21 +60,43 @@ combine_drawing_files () {
 ####################################################
 echo "Changing title metadata in pdf files..."
 
-change_drawing_title "clkv3-dwg-01" "Display - RGB digit PCB" 
-change_drawing_title "clkv3-dwg-02" "Display - Frame" 
-change_drawing_title "clkv3-dwg-03" "Display - Back cover" 
-change_drawing_title "clkv3-dwg-04" "Display - Front cover" 
-change_drawing_title "clkv3-dwg-05" "Display - RGB dots PCB"
-change_drawing_title "clkv3-dwg-06" "Touch keypad PCB" 
-change_drawing_title "clkv3-dwg-07" "Touch keypad overlay"
-change_drawing_title "clkv3-dwg-20" "ASSY - Display panel" 
+change_drawing_title "clkv3-dwg-01_rgb-digits-pcb" "Display - RGB digit PCB"
+change_drawing_title "clkv3-dwg-02_display-frame-front" "Display - Frame - Front"
+change_drawing_title "clkv3-dwg-03_display-front-cover" "Display - Front cover"
+change_drawing_title "clkv3-dwg-04_display-frame-back" "Display - Frame - Back"
+change_drawing_title "clkv3-dwg-05_display-back-cover" "Display - Back cover"
+change_drawing_title "clkv3-dwg-06_rgb-dots-pcb" "Display - RGB dots PCB"
+change_drawing_title "clkv3-dwg-07_touch-keypad-pcb" "Touch keypad PCB"
+change_drawing_title "clkv3-dwg-08_touch-keypad-overlay" "Touch keypad overlay"
+change_drawing_title "clkv3-dwg-09_rgb-lamp-pcb" "RGB lamp PCB"
+change_drawing_title "clkv3-dwg-10_enclosure-lower-section" "Enclosure - Lower section"
+change_drawing_title "clkv3-dwg-11_enclosure-upper-section" "Enclosure - Upper section"
+change_drawing_title "clkv3-dwg-12_front-panel-pcb" "Front panel PCB"
+change_drawing_title "clkv3-dwg-13_enclosure-back-panel" "Enclosure - Back panel"
+change_drawing_title "clkv3-dwg-14_motherboard-pcb" "Motherboard PCB"
+change_drawing_title "clkv3-dwg-15_daughterboard-pcb" "Daughterboard PCB"
+change_drawing_title "clkv3-dwg-16_psu-pcb" "PSU PCB"
+change_drawing_title "clkv3-dwg-17_alarm-switch-pcb" "Alarm switch PCB"
+change_drawing_title "clkv3-dwg-18_rtc-battery-door" "RTC Battery door"
+change_drawing_title "clkv3-dwg-19_rgb-lamp-window-frame" "RGB lamp window - Frame"
+change_drawing_title "clkv3-dwg-20_rgb-lamp-window-cover" "RGB lamp window - Cover"
+change_drawing_title "clkv3-dwg-21_front-panel-window" "Front panel window"
+change_drawing_title "clkv3-dwg-30_assy-display-frame-front" "ASSY - Display frame - Front"
+change_drawing_title "clkv3-dwg-31_assy-display-frame-back" "ASSY - Display frame - Back"
+change_drawing_title "clkv3-dwg-32_assy-display" "ASSY - Display panel"
+change_drawing_title "clkv3-dwg-33_assy-motherboard" "ASSY - Motherboard PCB"
+change_drawing_title "clkv3-dwg-34_assy-touch-keypad" "ASSY - Touch keypad"
+change_drawing_title "clkv3-dwg-35_final-assembly" "Final assembly"
+change_drawing_title "clkv3-dwg-50_dev-jig" "Jig - dev."
+change_drawing_title "clkv3-dwg-51_motherboard-assy-jig" "Motherboard assembly jig"
+
 
 ####################################################
-echo "Combining pdf files for model V3..."
+echo "Combining pdf files..."
 
-combine_drawing_files "clkv3-dwg" "01,02,03,04,05,06,07,20"
+combine_drawing_files "clkv3-dwg" "01,06,02,03,07,08,34,30,04,05,09,19,20,31,32,10,11,18,13,21,17,14,15,16,12,33,35"
 change_drawing_title "clkv3-dwg" "Alarm clock V3"
 
 
-echo        
+echo
 echo "Done"
