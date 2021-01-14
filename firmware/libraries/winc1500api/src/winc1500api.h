165ae556e236f59a9c03ccc2e4e539a9482b40b5
Benoit Frigon <benoit@frigon.info>
2021-01-14 15:36:13 -0500

    Added WINC1500C API library

---
+++ firmware/libraries/winc1500api/src/winc1500api.h
@@ -0,0 +1,30 @@
+//******************************************************************************
+//
+// Project : Alarm Clock V3
+// File    : lib/winc1500api/winc1500api.h
+// Author  : Benoit Frigon <www.bfrigon.com>
+//
+// -----------------------------------------------------------------------------
+//
+// This work is licensed under the Creative Commons Attribution-ShareAlike 4.0
+// International License. To view a copy of this license, visit
+//
+// http://creativecommons.org/licenses/by-sa/4.0/
+//
+// or send a letter to Creative Commons,
+// PO Box 1866, Mountain View, CA 94042, USA.
+//
+//******************************************************************************
+#ifndef WINC1500API_H
+#define WINC1500API_H
+
+extern "C" {
+    #include "driver/include/m2m_wifi.h"
+    #include "bsp/include/nm_bsp_arduino.h"
+    #include "driver/include/m2m_ssl.h"
+    #include "socket/source/socket_internal.h"
+}
+
+
+
+#endif /* WINC1500API_H */
